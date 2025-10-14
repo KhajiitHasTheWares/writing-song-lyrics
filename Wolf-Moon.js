1b4aeb8fd74a0f6edce579943fd86a1b93affd59
Finn <114116392+KhajiitHasTheWares@users.noreply.github.com>
2025-10-14 14:55:56 -0400

    Created Wolf-Moon.js

---
+++ Wolf-Moon.js
@@ -0,0 +1,16 @@
+const expr = "Wolf Moon - Type O Negative";
+switch (expr) {
+  case "Line 1, verse 2":
+    console.log("Unholy water");
+    break;
+  case "Line 2, verse 2":
+    console.log("Sanguine addiction");
+    break;
+  case "Line 3, verse 2":
+    console.log("Those silver bullets");
+    break;
+  case "Line 4, verse 2":
+    console.log("A last blood benidiction");
+  default:
+    console.log("Sorry, the line you tried to run didn't read");
+}
