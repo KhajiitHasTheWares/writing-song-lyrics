5b474fc412529abfb68764691dd1d31dc3999208
Finn <114116392+KhajiitHasTheWares@users.noreply.github.com>
2025-10-15 12:55:14 -0400

    Create king-for-a-day.js

---
+++ king-for-a-day.js
@@ -0,0 +1,16 @@
+const startSong = "king For A Day - Pierce The Veil";
+switch (startSong) {
+  case "Line 1, verse 3":
+    console.log("You told me think about it, well I did");
+    break;
+  case "Line 2, verse 3":
+    console.log("Now I don't wanna feel a thing anymore");
+    break;
+  case "Line 3, verse 3":
+    console.log("I'm tired of begging for the things that I want");
+    break;
+  case "line 4, verse 3":
+    console.log("I'm over sleeping like a dog on the floor");
+  default:
+    console.log("The song you're looking for does not exist!";
+}
