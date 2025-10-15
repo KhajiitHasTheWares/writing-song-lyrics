e20922ee8c46a7ef1c96a1e02075dfa5039437c8
Finn <114116392+KhajiitHasTheWares@users.noreply.github.com>
2025-10-15 11:44:07 -0400

    Create out-of-style.js

---
+++ out-of-style.js
@@ -0,0 +1,16 @@
+const expr = "Out of style - Limp Bizkit";
+switch (songStart) {
+  case "Line 1, Chorus 1":
+    console.log("It's time to rock this motherfucker 'cause I'm always out of style (style)");
+    break;
+  case "Line 2, Chorus 1":
+    console.log("Never change my style 'cause my style is kinda fresh");
+    break;
+  case "Line 3, Chorus 1":
+    console.log("You bitches askin' for a sound check, check, one, two, bitch");
+  case "Line 4, Chorus 1":
+    console.log("You barely touch the ground, yeah (woo)");
+    break;
+  default:
+    console.log("The song you're looking for does not exist!");
+}
