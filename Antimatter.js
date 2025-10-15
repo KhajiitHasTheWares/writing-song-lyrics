d743e666bbcced111dc78bbfbb66a36792fd6ab4
Finn <114116392+KhajiitHasTheWares@users.noreply.github.com>
2025-10-15 09:34:28 -0400

    Create Antimatter.js

---
+++ Antimatter.js
@@ -0,0 +1,16 @@
+switch ("Antimatter- Silent Planet") {
+  case "Line 1, verse 1":
+    console.log("Silhouette, guilty conscience, barely hanging by a breath");
+    break;
+  case "line 2, verse 1":
+    console.log("Cut the lights and make me your oblivion");
+    break;
+  case "line 3, verse 1":
+    console.log("And I will follow through the phosphorescent streets");
+    break;
+  case "line 4 verse 1":
+    console.log("I'd give anything to be in over my head");
+    break;
+  default:
+    console.log("The song you're looking for doesn't exist");
+}
