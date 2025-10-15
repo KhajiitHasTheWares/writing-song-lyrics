861883f16faa8f9272d1cdf2237cc3b9d2f0240b
Finn <114116392+KhajiitHasTheWares@users.noreply.github.com>
2025-10-15 12:05:02 -0400

    Create country-boy.js

---
+++ country-boy.js
@@ -0,0 +1,19 @@
+const startSong = "Country Boy - Alan Jackson";
+switch (startSong) {
+  case "line 1, verse 2":
+    console.log("'Cause I'm a country boy, I've got a four-wheel drive");
+    break;
+  case "line 2, verse 2":
+    console.log("Climb in my bed, I'll take you for a ride");
+    break;
+  case "line 3, verse 2":
+    console.log("Up city streets, down country roads");
+    break;
+  case "line 4, verse 2":
+    console.log("I can get you where you need to go");
+    break;
+  case "line 5, verse 2":
+    console.log("'Cause I'm a country boy");
+  default:
+    console.log("The song you are looking for doesn't exist!");
+}
