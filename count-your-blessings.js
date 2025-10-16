0e7d814e3414910d7d86ddd7a7a3a36eb7e33099
Finn <114116392+KhajiitHasTheWares@users.noreply.github.com>
2025-10-16 08:01:17 -0400

    Created COUNT YOUR BLESSINGS
    No, it's not the BMTH one

---
+++ count-your-blessings.js
@@ -0,0 +1,17 @@
+const startSong = ("COUNT YOUR BLESSINGS - $uicideBoy$");
+switch (startSong) {
+  case "line 1, verse 9":
+    console.log("Ruby da f---in' Cherry got a big heart, yeah, I always flex that muscle");
+    break;
+  case "line 2, verse 9":
+    console.log("$crim so stoic, you might be lucky to get a f---in' chuckle (ay, ay)");
+    break;
+  case "line 3 verse 9":
+    console.log("That's my partner, that's my cousin (ayy, ayy), yeah, our family knows the struggle");
+    break;
+  case "line 4 verse 9":
+    console.log("Dug our graves together, and we threw away the f---in' shovels");
+    break;
+  default:
+    console.log("The song you requested doesn't exist!");
+}
