ca8e222ae0c20c62b8a9cc7e34122be8c08794aa
Finn <114116392+KhajiitHasTheWares@users.noreply.github.com>
2025-10-21 07:21:58 -0400

    Added Metal-Health.js

---
+++ metal-health.js
@@ -0,0 +1,17 @@
+const startSong = "Metal Health - Quiet Riot";
+switch (startSong) {
+  case "Line 1, verse 2":
+    console.log("Bang your head");
+    break;
+  case "Line 2, verse 2":
+    console.log("Metal health will drive you mad");
+    break;
+  case "line 3, verse 2":
+    console.log("Bang your head")
+    break;
+  case "line 4, verse 2":
+    console.log("Metal health will drive you mad, all right");
+    break;
+  default:
+    console.log("The song you are looking for doesn't exist!");
+}
