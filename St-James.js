a0093b7ae1e16cf4072ab1cc765eb7482e06b352
Finn <114116392+KhajiitHasTheWares@users.noreply.github.com>
2026-02-19 08:26:13 -0500

    Create St James lyrics switch statement

---
+++ St-James.js
@@ -0,0 +1,14 @@
+const startSong = "St James";
+switch (startSong) {
+  case "Line 1":
+    console.log("Sometimes our saints are sinners");
+    break;
+  case "Line 2":
+    console.log("They blur the lines to lead the way, their way");
+    break;
+  case "Line 3":
+    console.log("Raise hell and a glass of reverence");
+    break;
+  case "Line 4":
+    console.log("The fearless lives of our great saints - our saints");
+}
