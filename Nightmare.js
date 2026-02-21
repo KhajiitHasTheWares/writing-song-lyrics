1a65e08f2c9833e899a6d3832ebde4e6cc424566
Finn <114116392+KhajiitHasTheWares@users.noreply.github.com>
2026-02-21 10:39:24 -0500

    Add song lyrics switch case to Nightmare.js

---
+++ Nightmare.js
@@ -0,0 +1,14 @@
+const = startSong "Nightmare -Avenged Sevenfold";
+switch (startSong) {
+  case "Line 1":
+    console.log("You should have known, the price of evil");
+    break;
+  case "Line 2":
+    console.log("And it hurts to know that you belong here, yea");
+    break:
+  case "Line 3":
+    console.log("Oh, it your f-ckin nightmare!");
+    break:
+	case "line 4":
+		console.log("NOw your nightmare comes to life");
+}
