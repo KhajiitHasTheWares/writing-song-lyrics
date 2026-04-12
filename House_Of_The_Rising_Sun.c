5dbf0b384ba23fe960c1cecaf4b926dc7864233b
Finn <114116392+KhajiitHasTheWares@users.noreply.github.com>
2026-04-12 10:20:23 -0400

    Implement switch statement for House Of The Rising Sun (FFDP)

---
+++ House_Of_The_Rising_Sun.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+switch houseOfRisingSun {
+  case "Line one":
+  printf("Well, mother, tell your children");
+break;
+  case "line two":
+  printf("Never do what I have done");
+break;
+case "line three":
+  printf("Spend your life in sin and misery");
+break;
+case "Line four":
+  printf("In the House Of The Rising Sun");
+break;
+}
