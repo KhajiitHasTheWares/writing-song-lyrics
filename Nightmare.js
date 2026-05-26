4ccc1c65e9932e5a67b32ba763848f75c9d3cb12
Finn <k.f.russell.5824@gmail.com>
2026-05-26 01:12:09 -0400

    Update Nightmare.js
    Changed switch statement to if-else statement

--- Nightmare.js
+++ Nightmare.js
@@ -1,14 +1,11 @@
-const = startSong "Nightmare -Avenged Sevenfold";
-switch (startSong) {
-  case "Line 1":
-    console.log("You should have known, the price of evil");
-    break;
-  case "Line 2":
-    console.log("And it hurts to know that you belong here, yea");
-    break:
-  case "Line 3":
-    console.log("Oh, it your f-ckin nightmare!");
-    break:
-	case "line 4":
-		console.log("NOw your nightmare comes to life");
+function startSong("Nightmare - Avenged Sevenfold") {
+
+if (startSong) {
+	console.log("Dragged you down below");
+} else {
+	console.log("Down to the Devil's show");
+} else {
+	console.log("To be his guest forever");
+} else {
+	console.log("Peace of mind is less than never");
 }
