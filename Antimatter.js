60d9a164ecf0770d5524de798e686dbc943edba4
Finn <k.f.russell.5824@gmail.com>
2026-05-26 01:03:05 -0400

    Update Antimatter.js
    Changed from switch statement to if-else statement

--- Antimatter.js
+++ Antimatter.js
@@ -1,17 +1,11 @@
-const startSong = "Antimatter- Silent Planet";
-switch (startSong) {
-  case "Line 1, verse 1":
-    console.log("Silhouette, guilty conscience, barely hanging by a breath");
-    break;
-  case "line 2, verse 1":
-    console.log("Cut the lights and make me your oblivion");
-    break;
-  case "line 3, verse 1":
-    console.log("And I will follow through the phosphorescent streets");
-    break;
-  case "line 4 verse 1":
-    console.log("I'd give anything to be in over my head");
-    break;
-  default:
-    console.log("The song you're looking for doesn't exist");
+function startSong ("Antimatter - Silent Planet") {
+
+if (startSong) {
+  console.log("Silhouette, guilty conscience, barely hanging by a breath");
+} else {
+  console.log("Cut the lights and make me your oblivion");
+} else {
+  console.log("And I will follow through the phosphorescent streets");
+} else {
+  console.log("I'd give anything to be in over my head");
 }
