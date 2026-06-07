8ffa87689d7e6647b7700a7e706cf04514245958
Finn <k.f.russell.5824@gmail.com>
2026-06-07 15:31:39 -0400

    Update syntax Nightmare.js

--- Nightmare.js
+++ Nightmare.js
@@ -2,10 +2,10 @@ function startSong("Nightmare - Avenged Sevenfold") {
 
 if (startSong) {
 	console.log("Dragged you down below");
-} else {
+} else (startSong) {
 	console.log("Down to the Devil's show");
-} else {
+} else (startSong) {
 	console.log("To be his guest forever");
-} else {
+} else (startSong) {
 	console.log("Peace of mind is less than never");
 }
