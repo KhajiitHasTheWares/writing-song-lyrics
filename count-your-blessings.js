6f35b5a10da4f239cf4dd7cbe90a548a2d3a3a66
Finn <k.f.russell.5824@gmail.com>
2026-06-07 10:16:22 -0400

    Update count-your-blessings.js to an if-else statement

--- count-your-blessings.js
+++ count-your-blessings.js
@@ -1,17 +1,11 @@
-const startSong = ("COUNT YOUR BLESSINGS - $uicideBoy$");
-switch (startSong) {
-  case "line 1, verse 9":
-    console.log("Ruby da f---in' Cherry got a big heart, yeah, I always flex that muscle");
-    break;
-  case "line 2, verse 9":
-    console.log("$crim so stoic, you might be lucky to get a f---in' chuckle (ay, ay)");
-    break;
-  case "line 3 verse 9":
-    console.log("That's my partner, that's my cousin (ayy, ayy), yeah, our family knows the struggle");
-    break;
-  case "line 4 verse 9":
-    console.log("Dug our graves together, and we threw away the f---in' shovels");
-    break;
-  default:
-    console.log("The song you requested doesn't exist!");
+function startSong ("COUNT YOUR BLESSINGS - $uicideBoy$") {
+
+if (startSong) {
+  console.log ("Ruby da f---in' Cherry got a big heart, yeah, I always flex that muscle");
+} else {
+  console.log ("$crim so stoic, you might be lucky to get a f---in' chuckle (ay, ay)");
+} else {
+  console.log ("That's my partner, that's my cousin (ayy, ayy), yeah, our family knows the struggle");
+} else {
+  console.log ("Dug our graves together, and we threw away the fuckin' shovels");
 }
