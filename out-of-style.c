b71ce80ed5a60dbaad4e4f23c659358818514ad5
Finn <k.f.russell.5824@gmail.com>
2026-06-07 15:53:32 -0400

    Update and rename out-of-style.js to out-of-style.c
    rewritten to an if-else statement

---
+++ out-of-style.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+  const char "Out Of Style";
+if(strcmp("Out Of Style", "Line One") == 0) {
+printf("It's time to rock this motherf---er 'cause I'm always out of style");
+}
+else if(strcmp("Out Of Style", "Line Two") == 0) {
+printf("Never change my style 'cause my style is kinda fresh");
+}
+else if(strcmp("Out Of Style", "Line Three") == 0) {
+printf("You b---hes askin' for a sound check, check, one, two, b---h");
+}
+else if(strcmp("Out Of Style", "Line Four") == 0) {
+printf("You barely touch the ground, yeah");
+}
+
+return 0;
+}
