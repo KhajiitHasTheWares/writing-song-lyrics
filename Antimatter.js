562ed4dec312560fc2709aa98aa0bf11bd95549b
Finn <k.f.russell.5824@gmail.com>
2026-06-07 15:30:53 -0400

    Update syntax Antimatter.js

--- Antimatter.js
+++ Antimatter.js
@@ -2,10 +2,10 @@ function startSong ("Antimatter - Silent Planet") {
 
 if (startSong) {
   console.log("Silhouette, guilty conscience, barely hanging by a breath");
-} else {
+} else (startSong) {
   console.log("Cut the lights and make me your oblivion");
-} else {
+} else (startSong) {
   console.log("And I will follow through the phosphorescent streets");
-} else {
+} else (startSong) {
   console.log("I'd give anything to be in over my head");
 }
