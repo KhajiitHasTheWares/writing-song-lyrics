469c8103e48fa994fd9f2d335faf6b3ca07d52b9
Finn <k.f.russell.5824@gmail.com>
2026-06-07 15:27:27 -0400

    Update king-for-a-day.js

--- king-for-a-day.js
+++ king-for-a-day.js
@@ -1,10 +1,10 @@
 function startSong ("king for a day - Pierce The Veil") {
   if (startSong) {
-    console.log ("You told me think about well I did");
+    console.log("You told me think about well I did");
   } else {
-    console.log ("Now I don't wanna feel a thing anymore");
+    console.log("Now I don't wanna feel a thing anymore");
   } else {
-    console.log ("I'm tired of begging for the things that I want");
+    console.log("I'm tired of begging for the things that I want");
   } else {
-    console.log ("I'm over sleeping like a dog on the floor");
+    console.log("I'm over sleeping like a dog on the floor");
   }
