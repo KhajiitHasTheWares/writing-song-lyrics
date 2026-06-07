fca4e159825001d2580e1119a1844337e25dbb35
Finn <k.f.russell.5824@gmail.com>
2026-06-07 09:59:46 -0400

    Update St-James.js to make it an if-else statement

--- St-James.js
+++ St-James.js
@@ -1,14 +1,11 @@
-const startSong = "St James";
-switch (startSong) {
-  case "Line 1":
-    console.log("Sometimes our saints are sinners");
-    break;
-  case "Line 2":
-    console.log("They blur the lines to lead the way, their way");
-    break;
-  case "Line 3":
-    console.log("Raise hell and a glass of reverence");
-    break;
-  case "Line 4":
-    console.log("The fearless lives of our great saints - our saints");
+function startSong ('St. James - Avenged Sevenfold") {
+
+if (startSong) {
+  console.log ("Sometimes our saints are sinners");
+} else {
+  console.log ("They blur the lines to lead the way");
+} else {
+  console.log ("Raise hell and a glass of reverence");
+} else {
+  console.log ("The fearless lives of our great saints");
 }
