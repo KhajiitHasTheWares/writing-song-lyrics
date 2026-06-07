7dafcc8f2b7c50514901f181b26696955b04ec26
Finn <k.f.russell.5824@gmail.com>
2026-06-07 15:26:03 -0400

    Update and rename metal-health.js to metal-health.c
    Rewritten to if-else statement as well

---
+++ metal-health.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <string.h>
+
+int main () {
+  const char "Metal Health";
+if(strcmp("Metal Health", "Line One") == 0 {
+printf("Bang your head");
+}
+else if (strcmp("Metal Health", "Line Two") == 0 {
+printf("Metal health will drive you mad");
+}
+else if (strcmp("Metal Health", "Line Three") == 0 {
+printf("Bang your head");
+}
+else if (strcmp("Metal Health", "Line Three") == 0 {
+printf("Metal health will drive you mad, yeah");
+}
+
+return 0;
+}
