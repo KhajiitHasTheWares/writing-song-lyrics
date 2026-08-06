c214e40a28ef3ffd2fc5692d0f59ef91c0e6f892
Finn <k.f.russell.5824@gmail.com>
2026-08-05 20:08:07 -0400

    Update and fully censored count-your-blessings.js

--- count-your-blessings.js
+++ count-your-blessings.js
@@ -7,5 +7,8 @@ if (startSong) {
 } else {
   console.log ("That's my partner, that's my cousin (ayy, ayy), yeah, our family knows the struggle");
 } else {
-  console.log ("Dug our graves together, and we threw away the fuckin' shovels");
+  console.log ("Dug our graves together, and we threw away the f----n' shovels");
+}
+
+return 0;
 }
